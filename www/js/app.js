14cb2c72dea68ec25eb8265dc5c60c3206f68f83
Shahzada Azam <shahzadaazam@mail.usf.edu>
2017-05-11 01:39:25 -0400

    Moving gallery code to controller and trying various experiments to make gallery work

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('ridesmart', ['ionic'])
+angular.module('ridesmart', ['ionic', 'ngCordova'])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
@@ -65,12 +65,56 @@ angular.module('ridesmart', ['ionic'])
 
 }])
 
-.controller('GalleryController', ['$scope', function($scope) {
+.controller('GalleryController', function($scope, $ionicPlatform, $cordovaCamera) {
 
-  $scope.galleryLoad = function(){
-    //Hiding ezar camera for gallery
+  $scope.photos = [];
+
+  $scope.getPhoto = function(){
     var x = document.getElementById("main");
     x.style.backgroundColor = "white";
+
+    var srcType = navigator.camera.PictureSourceType.SAVEDPHOTOALBUM;
+
+    var options = {
+        // Some common settings are 20, 50, and 100
+        quality: 50,
+        destinationType: navigator.camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery
+        sourceType: srcType,
+        encodingType: navigator.camera.EncodingType.JPEG,
+        mediaType: navigator.camera.MediaType.PICTURE,
+        allowEdit: false,
+        correctOrientation: true  //Corrects Android orientation quirks
+    }
+
+    navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+        console.log(imageUri);
+        // $scope.photos.unshift(imageUri);
+        alert('Image URL is: ' + imageUri);
+        alert('Scope photos: ' + $scope.photos);
+        //var image = document.getElementById ('picture');
+        //image.src = "data:image/jpeg;base64," + imageUri;
+        //displayImage(imageUri);
+        // You may choose to copy the picture, save it somewhere, or upload.
+        //func(imageUri);
+
+    }, function cameraError(error) {
+        console.debug("Unable to obtain picture: " + error, "app");
+
+    }, options);
+
+    // //Hiding ezar camera for gallery
+    // $ionicPlatform.ready(function(){
+    //   $cordovaCamera.getPicture({
+    //     destinationType: navigator.camera.DestinationType.FILE_URL,
+    //     sourceType: navigator.camera.PictureSourceType[type.toUpperCase()]
+    //   }).then(function(photo){
+    //     $scope.photos.unshift(photo);
+    //   }, function(err){
+    //     console.log(err);
+    //   });
+    // });
   }
 
-}]);
+});
