89d5d84f86c0315f3486809c3fe0fb704da752cc
Shahzada Azam <shahzadaazam@mail.usf.edu>
2017-05-12 01:54:50 -0400

    Fixed gallery button background, icon and scaling

--- www/js/app.js
+++ www/js/app.js
@@ -43,11 +43,29 @@ angular.module('ridesmart', ['ionic', 'ngCordova'])
   });
 })
 
-.controller('MasksController', ['$scope', function($scope) {
+.controller('MasksController', ['$scope', function($scope, $ionicPlatform) {
 
   //Making ezar video overlay visible on view load
   var x = document.getElementById("main");
   x.style.backgroundColor = "transparent";
+
+  console.log("local storage length is:" + window.localStorage.length);
+
+  if (window.localStorage.length > 0){
+    //Setting background image of gallery button to latest image
+    var key = window.localStorage.key(window.localStorage.length - 1);
+    var galleryButton = document.getElementById("gallerybutton");
+    galleryButton.style.backgroundImage = 'url(/storage/emulated/0/Pictures/' + window.localStorage.getItem(key) + '.jpg)';
+    galleryButton.style.border = 'none';
+    console.log(galleryButton.style.backgroundImage);
+
+    var galleryIcon = document.getElementById("galleryicon").className = 'none';
+  }
+  else{
+    var galleryButton = document.getElementById("gallerybutton");
+    galleryButton.style.backgroundImage = 'none';
+    console.log(galleryButton.style.backgroundImage);
+  }
 
   $scope.masks = [
     {id: '1', name: 'Ridesmart Shades', class: 'shades'},
@@ -83,12 +101,13 @@ angular.module('ridesmart', ['ionic', 'ngCordova'])
     // $scope.photos.push({id: i, src: t + "1(" + i + ").jpg"});
 
     for(var i = 0; i < window.localStorage.length; i++) {
-      key = window.localStorage.key(i);
+      var key = window.localStorage.key(i);
       console.log(key);
       //change the following path to pick specific images. Have to change the following to pick all images from the album
-    $scope.photos.push({id: i, src: "/storage/emulated/0/Pictures/" + window.localStorage.getItem(key) + ".jpg"});
+      $scope.photos.push({id: i, src: "/storage/emulated/0/Pictures/" + window.localStorage.getItem(key) + ".jpg"});
 
     }
+
   }
 
 });
